e903236b21fce5dd68d8c2dc8210a54ded6953fd
Martin Olarte <MartinO@Martins-MacBook-Pro-2.local>
2018-12-05 17:14:38 -0500

    Created R file with B1S data highlights.

--- data/B1-SData.R
+++ data/B1-SData.R
@@ -2,51 +2,33 @@ library("dplyr")
 library("ggplot2")
 library(readxl)
 library(gridExtra)
-B1_S <- read_excel("~/Desktop/Electronegatividad.xlsx", 
+B1_S_hours <- read_excel("~/Desktop/Electronegatividad.xlsx", 
                                 col_types = c("text", "numeric", "numeric", 
                                 "numeric", "numeric", "numeric", "numeric"), skip = 2)
 
-B1_S_first_session <- B1_S[1:17,] %>% mutate(days_after = 0:16)
-B1_S_second_session <- B1_S[19:30,] %>% mutate(days_after = 0:11)
-B1_S_third_session <- B1_S[32:37,] %>% mutate(days_after = 0:5)
+B1_S_dates <- read_excel("~/Desktop/PATIENT_DATA.xlsx", 
+                         col_types = c("date", "numeric", "numeric"))
 
-x_max <- max(c(max(B1_S_first_session$days_after), max(B1_S_second_session$days_after)))
-y_max1 <- max(B1_S_first_session$`Polarity level`, na.rm = T)
 
-f1 <- max(B1_S_first_session$days_after) + 1
-s1 <- max(B1_S_second_session$days_after) + 1
-t1 <- max(B1_S_third_session$days_after)
+B1_S_first_hours <- B1_S[1:17, c(2,7)]
+B1_S_second_hours <- B1_S[19:30, c(2,7)]
+B1_S_third_hours <- B1_S[32:37, c(2,7)]
+B1_S_first_hours[,2] <- cumsum(B1_S_first_hours[,2])
+B1_S_second_hours[,2] <- cumsum(B1_S_second_hours[,2])
+B1_S_third_hours[,2] <- cumsum(B1_S_third_hours[,2])
 
-B1_S_first_progress <- B1_S_first_session %>% select(days_after, `Polarity level`) %>%
-  ggplot(aes(x=days_after, y= `Polarity level`)) + geom_point() + 
-  scale_x_continuous(limits = c(0, x_max), breaks = pretty_breaks()) +
-  scale_y_continuous(limits = c(0, y_max1)) +
-  labs(subtitle = "First Session Results", x = "Days of Treatment", y = "Polarity Level") +
-  geom_smooth(size = 0.5) +
-  geom_text(data = B1_S_first_session[c(1,f1),c(8,2)],
-            label = B1_S_first_session$`Polarity level`[c(1, f1)],
-            nudge_y = -6)
+maxP1 <- max(B1_S_first_hours$`Polarity level`, na.rm = T)
+minP1 <- min(B1_S_first_hours$`Polarity level`, na.rm = T)
+maxP2 <- max(B1_S_second_hours$`Polarity level`, na.rm = T)
+minP2 <- min(B1_S_second_hours$`Polarity level`, na.rm = T)
+maxP3 <- max(B1_S_third_hours$`Polarity level`, na.rm = T)
+minP3 <- min(B1_S_third_hours$`Polarity level`, na.rm = T)
 
-B1_S_second_progress <- B1_S_second_session %>% select(days_after, `Polarity level`) %>%
-  ggplot(aes(x=days_after, y= `Polarity level`)) + geom_point() + 
-  scale_x_continuous(limits = c(0, x_max), breaks = pretty_breaks()) +
-  scale_y_continuous(limits = c(0, y_max1)) +
-  labs(subtitle = "Second Session Results", x = "Days of Treatment", y = "Polarity Level") +
-  geom_smooth(size = 0.5) +
-  geom_text(data = B1_S_second_session[c(1,s1),c(8,2)],
-            label = B1_S_second_session$`Polarity level`[c(1, s1)],
-            nudge_y = 5,
-            nudge_x = 0.8)
+totalB1SHours <- c(max(B1_S_first_hours$`Total therapy duration (Hrs)`, na.rm = T),
+                   max(B1_S_second_hours$`Polarity level`, na.rm = T),
+                   max(B1_S_third_hours$`Polarity level`, na.rm = T))
 
-B1_S_third_progress <- B1_S_third_session %>% select(days_after, `Polarity level`) %>%
-  ggplot(aes(x=days_after, y= `Polarity level`)) + geom_point() + 
-  scale_x_continuous(limits = c(0, x_max), breaks = pretty_breaks()) +
-  scale_y_continuous(limits = c(0, y_max1)) +
-  labs(subtitle = "Third Session Results", x = "Days of Treatment", y = "Polarity Level") +
-  geom_smooth(size = 0.5) +
-  geom_text(data = B1_S_third_session[c(2,t1),c(8,2)],
-            label = B1_S_third_session$`Polarity level`[c(2, t1)],
-            nudge_y = -6)
-
-grid.arrange(top = "B1-S Polarity", B1_S_first_progress, B1_S_second_progress, B1_S_third_progress, ncol=3)
+data.frame(Session = c("1st", "2nd", "3rd"), Hours = totalB1SHours,
+           Max_Polarity = c(maxP1,maxP2,maxP3), Min_Polarity = c(minP1,minP2,minP3),
+           Change = c(maxP1-minP1, maxP2-minP2, maxP3-minP3))
 
