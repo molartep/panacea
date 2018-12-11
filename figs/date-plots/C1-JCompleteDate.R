b506e0581fc08fde297f5addd24760334d8a8b1b
Martin Olarte <MartinO@Martins-MacBook-Pro-2.local>
2018-12-11 12:02:30 -0500

    Incorporated L3K's third break information.

--- figs/date-plots/C1-JCompleteDate.R
+++ figs/date-plots/C1-JCompleteDate.R
@@ -6,18 +6,18 @@ library(scales)
 C1J <- read_excel("~/Desktop/PATIENT_DATA.xlsx", sheet = "Jim", 
                   col_types = c("date", "numeric", "numeric"))
 
-lims <- as.POSIXct(strptime(c("2018-08-15 01:00","2018-12-01 01:00"), format = "%Y-%m-%d %H:%M"))
+lims <- as.POSIXct(strptime(c("2018-08-15 01:00","2018-12-15 01:00"), format = "%Y-%m-%d %H:%M"))
 
 C1J_dates <- C1J[,c(1,3)]
 
 C1J_max <- max(C1J$p_level, na.rm = T)
 
-#L2-E DATE GRAPH
+#C1-J DATE GRAPH
 C1J_dategraph <- C1J_dates %>%
   ggplot(aes(x=date, y= p_level)) + geom_point() + 
   scale_x_datetime(date_breaks = "1 month", date_labels =  "%b %Y", limits = lims) +
   scale_y_continuous(limits = c(0, C1J_max)) +
-  labs(subtitle = "L2-E", x = "",  y = "") +
+  labs(subtitle = "C1-J", x = "",  y = "") +
   #geom_smooth(size = 0.5) + 
   theme(axis.text.x = element_text(angle = 90, hjust = 1))
 
@@ -31,6 +31,11 @@ C1J_segment2 <- list(x1 = C1J_dates$date[42], y1 = C1J_dates$p_level[42],
 C1J_lab2x <- as.POSIXct((as.numeric(C1J_segment2$x1) + as.numeric(C1J_segment2$x2)) / 2, origin = '1970-01-01')
 C1J_lab2y <- (C1J_segment2$y1 + C1J_segment2$y2)/2
 
+C1J_segment3 <- list(x1 = C1J_dates$date[63], y1 = C1J_dates$p_level[63],
+                     x2 = C1J_dates$date[74], y2 = C1J_dates$p_level[74]) 
+C1J_lab3x <- as.POSIXct((as.numeric(C1J_segment3$x1) + as.numeric(C1J_segment3$x2)) / 2, origin = '1970-01-01')
+C1J_lab3y <- (C1J_segment3$y1 + C1J_segment3$y2)/2
+
 C1J_dategraph + geom_segment(aes(x = C1J_segment1$x1, y = C1J_segment1$y1,
                                  xend = C1J_segment1$x2, yend = C1J_segment1$y2, color = "9"),
                              linetype = "dashed") +
@@ -40,4 +45,9 @@ C1J_dategraph + geom_segment(aes(x = C1J_segment1$x1, y = C1J_segment1$y1,
                    xend = C1J_segment2$x2, yend = C1J_segment2$y2, color = "9 "),
                linetype = "dashed") +
   geom_text(aes(x = C1J_lab2x, y = C1J_lab2y + 5, color = "9 ", label = "+ 19.1",
-                angle = 30)) + labs(color = "Days Without\nTreatment")
+                angle = 30)) +
+  geom_segment(aes(x = C1J_segment3$x1, y = C1J_segment3$y1,
+                   xend = C1J_segment3$x2, yend = C1J_segment3$y2, color = "10"),
+               linetype = "dashed") +
+  geom_text(aes(x = C1J_lab3x, y = C1J_lab3y + 5, color = "10", label = "+ 1.1",
+                angle = 1)) + labs(color = "Days Without\nTreatment")
