902f466317f75db0704fcb3f7d1d297e06e1966a
Martin Olarte <MartinO@Martins-MacBook-Pro-2.local>
2019-02-07 11:10:31 -0500

    Renamed some variables, changed the method for the regression model to linear, and adjusted the labels for B1S's data.

--- figs/hour-plots/B1-STherapyHours.R
+++ figs/hour-plots/B1-STherapyHours.R
@@ -2,53 +2,53 @@ library("dplyr")
 library("ggplot2")
 library(readxl)
 library(gridExtra)
-B1_S <- read_excel("~/Desktop/Electronegatividad.xlsx", 
-                   col_types = c("text", "numeric", "numeric", 
+B1_S_hours <- read_excel("~/Desktop/Electronegatividad.xlsx", sheet = "Stephanie",
+                   col_types = c("date", "numeric", "numeric", 
                                  "numeric", "numeric", "numeric", "numeric"), skip = 2)
 
-B1_S_first_session <- B1_S[1:17,] %>% mutate(days_after = 0:16)
-B1_S_second_session <- B1_S[19:30,] %>% mutate(days_after = 0:11)
-B1_S_third_session <- B1_S[32:37,] %>% mutate(days_after = 0:5)
+first_B1_S <- B1_S_hours[1:17,] %>% mutate(days_after = 0:16)
+second_B1_S <- B1_S_hours[19:30,] %>% mutate(days_after = 0:11)
+third_B1_S <- B1_S_hours[32:37,] %>% mutate(days_after = 0:5)
 
-y_max1 <- max(B1_S_first_session$`Polarity level`, na.rm = T)
+y_max1 <- max(first_B1_S$`Polarity level`, na.rm = T)
 
-f1 <- max(B1_S_first_session$days_after) + 1
-s1 <- max(B1_S_second_session$days_after) + 1
-t1 <- max(B1_S_third_session$days_after)
+f1 <- max(first_B1_S[complete.cases(first_B1_S[,2]),]$days_after) + 1
+s1 <- max(second_B1_S[complete.cases(second_B1_S[,2]),]$days_after) + 1
+t1 <- max(third_B1_S[complete.cases(third_B1_S[,2]),]$days_after) + 1
 
-B1_S_first_session[,7] <- cumsum(B1_S_first_session[,7])
-B1_S_second_session[,7] <- cumsum(B1_S_second_session[,7])
-B1_S_third_session[,7] <- cumsum(B1_S_third_session[,7])
+first_B1_S[,7] <- cumsum(first_B1_S[,7])
+second_B1_S[,7] <- cumsum(second_B1_S[,7])
+third_B1_S[,7] <- cumsum(third_B1_S[,7])
 
-B1_S_first_progress <- B1_S_first_session %>% select(`Total therapy duration (Hrs)`, `Polarity level`) %>%
+B1_S_first_progress <- first_B1_S %>% select(`Total therapy duration (Hrs)`, `Polarity level`) %>%
   ggplot(aes(x=`Total therapy duration (Hrs)`, y= `Polarity level`)) + geom_point() + 
   scale_x_continuous(breaks = pretty_breaks()) +
   scale_y_continuous(limits = c(0, y_max1)) +
   labs(subtitle = "First Session Results", x = "Total Therapy Duration (Hrs)", y = "Polarity Level") +
-  geom_smooth(size = 0.5) +
-  geom_text(data = B1_S_first_session[c(1,f1),c(7,2)],
-            label = B1_S_first_session$`Polarity level`[c(1, f1)],
+  geom_smooth(method = "lm", size = 0.5) +
+  geom_text(data = first_B1_S[c(1,f1),c(7,2)],
+            label = first_B1_S$`Polarity level`[c(1, f1)],
             nudge_y = -12)
 
-B1_S_second_progress <- B1_S_second_session %>% select(`Total therapy duration (Hrs)`, `Polarity level`) %>%
+B1_S_second_progress <- second_B1_S %>% select(`Total therapy duration (Hrs)`, `Polarity level`) %>%
   ggplot(aes(x=`Total therapy duration (Hrs)`, y= `Polarity level`)) + geom_point() + 
   scale_x_continuous(breaks = pretty_breaks()) +
   scale_y_continuous(limits = c(0, y_max1)) +
   labs(subtitle = "Second Session Results", x = "Total Therapy Duration (Hrs)", y = "Polarity Level") +
-  geom_smooth(size = 0.5) +
-  geom_text(data = B1_S_second_session[c(1,s1),c(7,2)],
-            label = B1_S_second_session$`Polarity level`[c(1, s1)],
+  geom_smooth(method = "lm", size = 0.5) +
+  geom_text(data = second_B1_S[c(1,s1),c(7,2)],
+            label = second_B1_S$`Polarity level`[c(1, s1)],
             nudge_y = 12,
             nudge_x = 0.8)
 
-B1_S_third_progress <- B1_S_third_session %>% select(`Total therapy duration (Hrs)`, `Polarity level`) %>%
+B1_S_third_progress <- third_B1_S %>% select(`Total therapy duration (Hrs)`, `Polarity level`) %>%
   ggplot(aes(x=`Total therapy duration (Hrs)`, y= `Polarity level`)) + geom_point() + 
   scale_x_continuous(breaks = pretty_breaks()) +
   scale_y_continuous(limits = c(0, y_max1)) +
   labs(subtitle = "Third Session Results", x = "Total Therapy Duration (Hrs)", y = "Polarity Level") +
-  geom_smooth(size = 0.5) +
-  geom_text(data = B1_S_third_session[c(2,t1),c(7,2)],
-            label = B1_S_third_session$`Polarity level`[c(2, t1)],
+  geom_smooth(method = "lm", size = 0.5) +
+  geom_text(data = third_B1_S[c(2,t1),c(7,2)],
+            label = third_B1_S$`Polarity level`[c(2, t1)],
             nudge_y = -12)
 
 grid.arrange(top = "B1-S Polarity", B1_S_first_progress, B1_S_second_progress, B1_S_third_progress, ncol=2)
