fe5038796302e40ac4bafe13e5ee6adb5641b6e5
Martin Olarte <MartinO@Martins-MacBook-Pro-2.local>
2019-02-13 12:32:25 -0500

    Included start and end dates, L3-K's fourth session information, and split the data into session and breaks.

--- data/L3-KData.R
+++ data/L3-KData.R
@@ -1,8 +1,7 @@
-library("dplyr")
-library("ggplot2")
+library(dplyr)
+library(ggplot2)
 library(readxl)
 library(gridExtra)
-library(bizdays)
 L3_K_hours <- read_excel("~/Desktop/Electronegatividad.xlsx", sheet = "Kevin",
                          col_types = c("date", "numeric", "numeric", 
                                        "numeric", "numeric", "numeric"), skip = 2)
@@ -14,14 +13,26 @@ L3_K_dates <- read_excel("~/Desktop/PATIENT_DATA.xlsx", sheet = "Kevin",
 L3_K_first_hours <- L3_K_hours[1:7, c(1,2,6)]
 L3_K_second_hours <- L3_K_hours[9:20, c(1,2,6)]
 L3_K_third_hours <- L3_K_hours[22:28, c(1,2,6)]
+L3_K_fourth_hours <- L3_K_hours[30:34, c(1,2,6)]
+
+start1 <- L3_K_first_hours[L3_K_first_hours[,3] > 0, 1][1,1]
+end1 <- tail(L3_K_first_hours[L3_K_first_hours[,3] > 0, 1], n = 1)
+start2 <- L3_K_second_hours[L3_K_second_hours[,3] > 0, 1][1,1]
+end2 <- tail(L3_K_second_hours[L3_K_second_hours[,3] > 0, 1], n = 1)
+start3 <- L3_K_third_hours[L3_K_third_hours[,3] > 0, 1][1,1]
+end3 <- tail(L3_K_third_hours[L3_K_third_hours[,3] > 0, 1], n = 1)
+start4 <- L3_K_fourth_hours[L3_K_fourth_hours[,3] > 0, 1][1,1]
+end4 <- tail(L3_K_fourth_hours[L3_K_fourth_hours[,3] > 0, 1], n = 1)
 
 L3K_dates1 <- sum(L3_K_first_hours[,3] > 0)
 L3K_dates2 <- sum(L3_K_second_hours[,3] > 0)
 L3K_dates3 <- sum(L3_K_third_hours[,3] > 0)
+L3K_dates4 <- sum(L3_K_fourth_hours[,3] > 0)
 
 L3_K_first_hours[,3] <- cumsum(L3_K_first_hours[,3])
 L3_K_second_hours[,3] <- cumsum(L3_K_second_hours[,3])
 L3_K_third_hours[,3] <- cumsum(L3_K_third_hours[,3])
+L3_K_fourth_hours[,3] <- cumsum(L3_K_fourth_hours[,3])
 
 maxP1 <- max(L3_K_first_hours$`Polarity level`, na.rm = T)
 minP1 <- min(L3_K_first_hours$`Polarity level`, na.rm = T)
@@ -29,27 +40,41 @@ maxP2 <- max(L3_K_second_hours$`Polarity level`, na.rm = T)
 minP2 <- min(L3_K_second_hours$`Polarity level`, na.rm = T)
 maxP3 <- max(L3_K_third_hours$`Polarity level`, na.rm = T)
 minP3 <- min(L3_K_third_hours$`Polarity level`, na.rm = T)
+maxP4 <- max(L3_K_fourth_hours$`Polarity level`, na.rm = T)
+minP4 <- min(L3_K_fourth_hours$`Polarity level`, na.rm = T)
 
 totalL3KHours <- c(max(L3_K_first_hours$`Total therapy duration (Hrs)`, na.rm = T),
-                   NA,
                    max(L3_K_second_hours$`Total therapy duration (Hrs)`, na.rm = T),
-                   NA,
                    max(L3_K_third_hours$`Total therapy duration (Hrs)`, na.rm = T),
-                   NA
-                   )
+                   max(L3_K_fourth_hours$`Total therapy duration (Hrs)`, na.rm = T))
 
 totalL3KDays <- c(L3K_dates1, 
-                  (L3_K_second_hours[L3_K_second_hours[,3] > 0, 1][1,1]-
-                     tail(L3_K_first_hours[L3_K_first_hours[,3] > 0, 1], n = 1))$Day,
+                  (start2 - end1)$Day,
                   L3K_dates2,
-                  (L3_K_third_hours[L3_K_third_hours[,3] > 0, 1][1,1]-
-                     tail(L3_K_second_hours[L3_K_second_hours[,3] > 0, 1], n = 1))$Day,
+                  (start3 - end2)$Day,
                   L3K_dates3,
-                  NA)
+                  (start4 - end3)$Day,
+                  L3K_dates4)
 
-data.frame(Interval = c("1st session", "1st break", "2nd session", "2nd break", "3rd session", "3rd break"),
-           Hours = totalL3KHours,
-           Days = totalL3KDays,
-           Polarity = c(maxP1,minP1,maxP2,minP2,maxP3,minP3),
-           Change = c(minP1-maxP1, maxP2-minP1, minP2-maxP2, maxP3-minP2, minP3-maxP3, NA))
+
+L3K_df_sessions <- data.frame(Interval = c("1st", "2nd", "3rd", "4th"),
+                              start_date = mapply(format, c(start1, start2, start3, start4), format = "%b %d %Y"),
+                              end_date = mapply(format, c(end1, end2, end3, end4), format = "%b %d %Y"),
+                              Hours = totalL3KHours,
+                              Days = totalL3KDays[seq(1, length(totalL3KDays), 2)],
+                              Starting_Polarity = c(maxP1,maxP2,maxP3,maxP4),
+                              Final_Polarity = c(minP1,minP2,minP3,minP4),
+                              Change = c(minP1-maxP1, minP2-maxP2, minP3-maxP3, minP4-maxP4))
+
+L3K_df_sessions <- L3K_df_sessions %>% mutate(Change_per_hr = round(Change/Hours, digits = 3))
+
+L3K_df_breaks <- data.frame(Interval = c("1st", "2nd", "3rd"),
+                            Days = totalL3KDays[seq(2, length(totalL3KDays), 2)],
+                            Increase_in_Polarity = c(maxP2-minP1, maxP3-minP2, maxP4-minP3))
+
+L3K_df_breaks <- L3K_df_breaks %>% mutate(Increase_per_day = round(Increase_in_Polarity/Days, digits = 3))
+
+L3K_df_sessions
+L3K_df_breaks
+
 
